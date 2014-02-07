289af01980af7f9c2762b1efe1f9a3888258dea3
Reed Weichler <rweichler@gmail.com>
2014-02-07 02:37:19 -0800

    remove redundant code from lua scripts

--- tweak/scripts/include/cube.lua
+++ tweak/scripts/include/cube.lua
@@ -1,17 +1,5 @@
 local M_PI = 3.14159265
 
-local function spin(view, percent)
-    local angle = percent*M_PI*2
-
-    local i = 0
-    while true do
-        i = i + 1
-        local v = view[i]
-        if v == nil then break end
-        v:rotate(BASE, angle, 0, 0, 1)
-    end
-end
-
 return function (view, percent, is_inside)
     local angle = percent*M_PI/2
     if not is_inside then angle = -angle end
