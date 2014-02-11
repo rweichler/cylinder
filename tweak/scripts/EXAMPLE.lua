6f0f0197952d7a215faf8325e89056f5461e13c0
Reed Weichler <rweichler@gmail.com>
2014-02-10 19:26:54 -0800

    example

--- tweak/scripts/EXAMPLE.lua
+++ tweak/scripts/EXAMPLE.lua
@@ -1,11 +1,4 @@
 --[[
-there is only one custom type, a view (UIView)
-view.subviews returns a table containing view's subviews (which conveniently are the individual icons)
-you can also do view[i] to get the subview at an index
-keep in mind, with lua, arrays start with index 1, not 0
-width is the width of the screen
-offset is the offset of the page from the screen's center.
-
 available functions:
 
 include("file.lua") --> runs the lua file like a function and returns whatever it returns
@@ -44,17 +37,43 @@ view.transform --> advanced users only. this is if you want to
 more will be added later
 ]]
 
+--declare your own constants and functions here
+
+PI = 3.14159265
+
+function abs(x) --> absolute value convenience function
+    if x < 0 then
+        return -x
+    else
+        return x
+    end
+end
+
 --this is the function that gets called when the screen moves
 --remember to "return" it at the end
 --"view" is the icon page you will be manipulating (aka a view)
 --"offset" is the x-offset of the current page to the center of the screen
 --"width" and "height" are the width and height of the screen
+
 return function(view, offset, width, height)
-
     local percent = offset/width
-    if percent < 0 then percent = -percent end
 
-    view:rotate(percent*3.14159265/4, 1, 0, 0) --this will tilt all icons slightly backward
+    view:rotate(percent*PI/3, 1, 0, 0) --> this will tilt the page slightly backward
+
+    local first_icon = view[1]
+    first_icon:rotate(percent*PI*2) --> this will spin the first icon in the page
+
+    local i = 0
+    while true do --> loop through all of the icons
+        i = i + 1
+        local icon = view[i]
+        if icon == nil then --> if there is no view
+            break --break out of the loop
+        else
+            icon.alpha = 1 - abs(percent) --> set the opacity with respect to how far away it is from the center of the screen
+                                          --  this calls the absolute value function we declared earlier
+        end
+    end
 end
 
 --errors are stored in /var/mobile/Library/Logs/Cylinder/errors.log
