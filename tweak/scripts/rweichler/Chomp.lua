adfcdb5401829c71c21cdce2d7f85fa0f702f6d9
Reed Weichler <rweichler@gmail.com>
2014-02-15 01:15:12 -0800

    made chomp flat (since you can just combine it with cube anyway)

--- tweak/scripts/rweichler/Chomp.lua
+++ tweak/scripts/rweichler/Chomp.lua
@@ -1,5 +1,3 @@
-local cube = dofile("include/cube.lua")
-
 local function chomp(view, percent, height)
     local oldp = percent
     if percent < 0 then percent = -percent end
@@ -11,7 +9,7 @@ local function chomp(view, percent, height)
         if v == nil then break end
 
         local mult = 1
-        if i <= 8 then mult = -1 end
+        if v.y + v.height/2 < (height - v.height)/2 then mult = -1 end
         v:translate(0, mult*percent*height/2, 0)
     end
 end
@@ -19,5 +17,4 @@ end
 return function(page, offset, width, height)
     local percent = offset/width
     chomp(page, percent, height)
-    cube(page, width, percent, true)
 end
