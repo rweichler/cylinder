95b37a9e12a85ba01e7b4e99544b617e78dddd10
Reed Weichler <rweichler@gmail.com>
2014-02-17 16:56:05 -0800

    spin hotfix

--- tweak/scripts/rweichler/Spin.lua
+++ tweak/scripts/rweichler/Spin.lua
@@ -1,12 +1,8 @@
-local function spin(view, percent)
+return function(page, offset, screen_width, screen_height)
+    local percent = offset/page.width
     local angle = percent*math.pi*2
 
     for i, icon in subviews(page) do
-        v:rotate(angle)
+        icon:rotate(angle)
     end
-end
-
-return function(page, offset, screen_width, screen_height)
-    local percent = offset/page.width
-    spin(page, percent)
 end
