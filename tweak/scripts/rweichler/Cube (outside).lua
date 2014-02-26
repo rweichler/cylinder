9edc4f3161af6f637ec0e4ff61cb72efbbc75010
Reed Weichler <rweichler@gmail.com>
2014-02-26 02:41:52 -0800

    removed redundant code

--- tweak/scripts/rweichler/Cube (outside).lua
+++ tweak/scripts/rweichler/Cube (outside).lua
@@ -1,11 +1,9 @@
 local cube = dofile("include/cube.lua")
 
 return function (page, offset, screen_width, screen_height)
-    local percent = offset/page.width
+    local percent = math.abs(offset/page.width)
 
     cube(page, offset, false)
-
-    if percent < 0 then percent = -percent end
 
     page.alpha = 1 - math.pow(percent, 3)
 end
