605f59bec77b1550a17b333cd145465f284d937d
Reed Weichler <rweichler@gmail.com>
2014-02-26 18:38:23 -0800

    fixed card effects

--- tweak/scripts/rweichler/Card (horizontal).lua
+++ tweak/scripts/rweichler/Card (horizontal).lua
@@ -2,7 +2,7 @@
 
 return function(page, offset, screen_width, screen_height)
 
-    page:translate(offset)
+    page.layer.x = page.layer.x + offset
 
     local percent = offset/page.width
 
@@ -10,11 +10,6 @@ return function(page, offset, screen_width, screen_height)
         page.alpha = 0
     end
 
-    local angle = math.pi
-    if offset ~= 0 then
-        angle = math.atan(PERSPECTIVE_DISTANCE/math.abs(offset))
-    end
-
-    page:rotate(angle*percent*2, 0, 1, 0)
+    page:rotate(-math.pi*percent, 0, 1, 0)
 
 end
--- tweak/scripts/rweichler/Card (vertical).lua
+++ tweak/scripts/rweichler/Card (vertical).lua
@@ -2,7 +2,7 @@
 
 return function(page, offset, screen_width, screen_height)
 
-    page:translate(offset)
+    page.layer.x = page.layer.x + offset
 
     local percent = offset/page.width
 
@@ -10,8 +10,6 @@ return function(page, offset, screen_width, screen_height)
         page.alpha = 0
     end
 
-    page:rotate(math.atan(-offset/PERSPECTIVE_DISTANCE), 0, 1, 0)
     page:rotate(math.pi*percent, 1, 0, 0)
-
 
 end
