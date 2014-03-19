a8aee0b3558e11bc8d14355635eeb612249a53e8
Reed Weichler <rweichler@gmail.com>
2014-03-18 17:56:26 -0700

    fixed page fade

--- tweak/scripts/rweichler/Page Fade.lua
+++ tweak/scripts/rweichler/Page Fade.lua
@@ -1,8 +1,12 @@
 
 return function(page, offset, screen_width, screen_height)
 
-    local percent = math.abs(offset/page.width)
+    local percent = math.abs(offset/page.layer.width)
 
     page.alpha = 1 - percent
+
+    for i, icon in subviews(page) do
+        icon.alpha = page.alpha
+    end
 
 end
