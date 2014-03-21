bff48baa276f1159585b5ac61450e604847d1cd2
Reed Weichler <rweichler@gmail.com>
2014-03-20 19:40:17 -0700

    update @beta382 scripts

--- tweak/scripts/Beta382/Rubik's Cube (complex).lua
+++ tweak/scripts/Beta382/Rubik's Cube (complex).lua
@@ -8,8 +8,7 @@ local seed = os.time()
  -- Do so if you want to find which seed you like
 --print (seed)
 
-
---cool seeds: 1394149256
+--cool seeds: 1394149256, 1394152451
 
 local function hasMatch(list, parameter)
     for i, item in ipairs(list) do
@@ -47,7 +46,7 @@ return function(page, offset, screen_width, screen_height)
             
             local isDupe = true
             while (isDupe) do
-                if(not hasMatch(firstStage, rowToSet)) then
+                if (not hasMatch(firstStage, rowToSet)) then
                     isDupe = false
                 else
                     rowToSet = math.random(0, page.max_rows-1)
@@ -153,12 +152,15 @@ return function(page, offset, screen_width, screen_height)
     local percent = offset/page.width
     
     -- A value from 0 to 1/3 that specifies how long each row should take to complete animating
-    -- Change this and only this, hackers. If you make it larger than 1/3, weird things will happen
+    -- Change this and only this, hackers. If you make it larger than 1/3, weird things will happen.
+    -- Although, that might be exactly what you want, so I'm not doing any bounds checking. Hack away
     local animationDuration = 1/3
     
+    local magic = (1-animationDuration)/2 -- Adapted from another script. Literally is magic
+    
     local stage1P = percent*(1/animationDuration)
-    local stage2P = (percent-1/3)*(1/animationDuration)
-    local stage3P = (percent-2/3)*(1/animationDuration)
+    local stage2P = (percent-magic)*(1/animationDuration)
+    local stage3P = (percent-2*magic)*(1/animationDuration)
     
     if (offset >= 0) then
         if (stage1P > 1) then stage1P = 1
@@ -170,8 +172,8 @@ return function(page, offset, screen_width, screen_height)
         if (stage3P > 1) then stage3P = 1
         elseif (stage3P < 0) then stage3P = 0 end
     else
-        stage1P = (percent+2/3)*(1/animationDuration)
-        stage2P = (percent+1/3)*(1/animationDuration)
+        stage1P = (percent+2*magic)*(1/animationDuration)
+        stage2P = (percent+magic)*(1/animationDuration)
         stage3P = percent*(1/animationDuration)
         
         if (stage1P < -1) then stage1P = -1
