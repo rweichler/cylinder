bf6fe9d322c210c9f73a8abde5f0fe568248e4b9
Reed Weichler <rweichler@gmail.com>
2014-12-28 13:41:16 -0800

    fix iOS < 6 springloop, performance improvements

--- tweak/lua_UIView.h
+++ tweak/lua_UIView.h
@@ -20,6 +20,7 @@ along with Cylinder.  If not, see <http://www.gnu.org/licenses/>.
 #import <lua/lua.h>
 #import <lua/lauxlib.h>
 #import "CALayer+Cylinder.h"
+#import <Defines.h>
 
 #define CHECK_UIVIEW(STATE, INDEX) \
     if(!lua_isuserdata(STATE, INDEX) || ![(NSObject *)lua_touserdata(STATE, INDEX) isKindOfClass:UIView.class]) \
@@ -29,7 +30,7 @@ along with Cylinder.  If not, see <http://www.gnu.org/licenses/>.
 //for transformations that translate, THEN rotate. (like cube,
 //page flip, etc)
 #define SCREEN_SIZE UIScreen.mainScreen.bounds.size
-#define PERSPECTIVE_DISTANCE ((SCREEN_SIZE.width +  SCREEN_SIZE.height)/2)
+#define PERSPECTIVE_DISTANCE ((SCREEN_SIZE.width +  SCREEN_SIZE.height)/2) 
 
 int l_create_uiview_metatable(lua_State *L);
 int l_push_base_transform(lua_State *L);
