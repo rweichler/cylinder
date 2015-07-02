13be5ce0f9de2893f61eb17d7b71d87b2c8ddca0
Reed Weichler <rweichler@gmail.com>
2015-07-01 18:19:36 -0700

    yeah dude like so basically like im gonna like fuckign like i basically changed the way the orer of the icons is, its basically like i do it more often but its in C so its hella fast but the issue is that it still doesnt work that well, but its less frequent than before on iOS 8

---
+++ tweak/icon_sort.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+int get_max_icons_for_list(UIView *self);
+UIView ** get_sorted_icons_from_list(id self);
+void sort_icons_for_list(UIView *self);
+void dealloc_sorted_icon_array_for_list(UIView *self);
