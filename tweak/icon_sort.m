8e48c1aa1e06eaa8d57eea46817add0c4d39723c
Reed Weichler <rweichler@gmail.com>
2015-07-01 18:19:36 -0700

    yeah dude like so basically like im gonna like fuckign like i basically changed the way the orer of the icons is, its basically like i do it more often but its in C so its hella fast but the issue is that it still doesnt work that well, but its less frequent than before on iOS 8

---
+++ tweak/icon_sort.m
@@ -0,0 +1,68 @@
+#import "icon_sort.h"
+#import <objc/runtime.h>
+
+//this could have been a single statement,
+//but id rather have it be readable
+static BOOL compare(UIView *a, UIView *b)
+{
+    CGPoint left = a.frame.origin;
+    CGPoint right = b.frame.origin;
+
+    return left.y < right.y || (left.y == right.y && left.x <= right.x);
+}
+
+static void insertion_sort(NSArray *subviews, UIView **arr, int max)
+{
+    int count = 0;
+    for(UIView *view in subviews) {
+        int i;
+        for(i = count - 1; i >= 0; i--) {
+            if(compare(arr[i], view)) {
+                break;
+            } else {
+                arr[i + 1] = arr[i];
+            }
+        }
+        arr[i + 1] = view;
+        count++;
+    }
+    memset(&arr[count], (int)NULL, (max - count)*sizeof(UIView *));
+}
+
+int get_max_icons_for_list(UIView *self)
+{
+    SEL sel = @selector(maxIcons);
+    typedef int (*func_t)(id, SEL);
+    return ((func_t)[self.class methodForSelector:sel])(self.class, sel);
+}
+
+UIView ** get_sorted_icons_from_list(id self)
+{
+    return [objc_getAssociatedObject(self, sort_icons_for_list) pointerValue];
+}
+
+static void set_obj(id self, id val)
+{
+    objc_setAssociatedObject(self, sort_icons_for_list, val, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+void sort_icons_for_list(UIView *self)
+{
+    int max = get_max_icons_for_list(self);
+
+    UIView **arr = get_sorted_icons_from_list(self);
+    if(arr == NULL) {
+        arr = malloc(max*sizeof(UIView *));
+        set_obj(self, [NSValue valueWithPointer:arr]);
+    }
+    insertion_sort(self.subviews, arr, max);
+}
+
+void dealloc_sorted_icon_array_for_list(UIView *self)
+{
+    void *val = get_sorted_icons_from_list(self);
+    if(val != NULL) {
+        free(val);
+        set_obj(self, nil);
+    }
+}
